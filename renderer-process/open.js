d30e0cc8764c0b875e28f665b029eed9ec7c67e1
daozhao <daozhao@163.com>
2018-03-22 16:20:10 +0800

    第 4 节    打开窗口 - BrowserWindow and open step 2

--- renderer-process/open.js
+++ renderer-process/open.js
@@ -1,5 +1,23 @@
 const click = document.querySelector('#click');
 
+const path = require('path');
+
+const BrowerWindow = require('electron').remote.BrowserWindow;
+
 click.onclick = () => {
-   window.open('http://nodelover.me');
+//    window.open('http://nodelover.me');
+    win = new BrowerWindow({
+        width: 300,
+        height: 200,
+        frame: true
+        // frame: false
+    })
+
+    win.on('close',()=>{win = null});
+
+    win.loadURL(path.join('file:',__dirname,'../model.html'));
+
+    console.dir(win);
+
+    //open('./model.html');
 }
