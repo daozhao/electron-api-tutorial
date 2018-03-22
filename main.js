891919ca346f64777f72a8eff4ae965c55bed449
daozhao <daozhao@163.com>
2018-03-22 15:41:23 +0800

    初此化，配置好vscode debug。

---
+++ main.js
@@ -0,0 +1,42 @@
+const {app,BrowserWindow} = require('electron');
+
+const path = require('path');
+
+const url = require('url');
+
+let win;
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    const URL = url.format({
+        pathname: path.join(__dirname,"index.html"),
+        protocol: 'file',
+        slashes: true
+    });
+
+    console.log(URL);
+    win.loadURL(URL);
+    win.webContents.openDevTools();
+    win.on('close',() => {
+        win = null;
+    });
+}
+
+app.on('ready',createWindow);
+
+app.on('window-all-close',()=>{
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
+
+app.on('activate',()=>{
+    if (win === null) {
+        createWindow()
+    }
+})
+
