7a973449af3cbab0eccba9ce44104148880abe5e
daozhao <daozhao@163.com>
2018-03-23 15:10:43 +0800

    第 10 节    弹出框 —step 2

--- main-process/dialog.js
+++ main-process/dialog.js
@@ -1,17 +1,32 @@
 const { dialog } = require('electron');
 
-dialog.showErrorBox('title','content');
+// dialog.showErrorBox('title','content');
 
 
-dialog.showMessageBox({
-    type:'info',
-    title: 'message',
-    message: 'hello',
-    buttons:['ok','cancel']
-},(index) => {
-    if ( index == 0 ) {
-        console.log('You click ok.');
-    } else {
-        console.log('You click cancel');
-    }
+// dialog.showMessageBox({
+//     type:'info',
+//     title: 'message',
+//     message: 'hello',
+//     buttons:['ok','cancel']
+// },(index) => {
+//     if ( index == 0 ) {
+//         console.log('You click ok.');
+//     } else {
+//         console.log('You click cancel');
+//     }
+// })
+
+dialog.showOpenDialog({
+    properties:['openFile','openDirectory']
+}, (files) => {
+    console.log(files);
+})
+
+dialog.showSaveDialog({
+    title: 'save some file',
+    filters: [
+        {name:'some',extensions:['js']}
+    ]
+},(filename) => {
+    console.log(filename);
 })
