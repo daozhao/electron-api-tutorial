d4583e19d4af12c0d04bebdc00c08bf880fcd1d9
daozhao <daozhao@163.com>
2018-03-23 15:36:42 +0800

    第 12 节    浏览器打开链接—step 2

--- renderer-process/link.js
+++ renderer-process/link.js
@@ -1,3 +1,15 @@
 const { shell } = require('electron');
 
-shell.openExternal('https://www.bing.com');
+// shell.openExternal('https://www.bing.com');
+
+const links = document.querySelectorAll('a[href]');
+
+Array.from(links).forEach((link) => {
+    const url = link.getAttribute('href');
+    if ( url.indexOf('http') == 0) {
+        link.onclick = (e) => {
+            e.preventDefault();
+            shell.openExternal(url);
+        }
+    }
+})
